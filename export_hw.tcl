e28a0045fbd7f7d4042f6b9506c93cadfcc390bc
Oleksandr Kiyenko <o.kiyenko@gmail.com>
2026-03-18 20:24:32 +0100

    Fix project name

--- export_hw.tcl
+++ export_hw.tcl
@@ -1,5 +1,6 @@
 puts "Exporting project $env(PROJECT_NAME)"
 set PROJECT_NAME $env(PROJECT_NAME)
+set XSA_FILE $env(XSA_FILE)
 
-open_project project/project.xpr
-write_hw_platform -fixed -include_bit -force -file ./project/TOP_wrapper.xsa
+open_project project/$PROJECT_NAME.xpr
+write_hw_platform -fixed -include_bit -force -file $XSA_FILE
